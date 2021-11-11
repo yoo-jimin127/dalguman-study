916fbb8238da10e334867c72723c811815816a88
yoo-jimin127 <dbwlals9936@naver.com>
2021-11-11 18:56:23 +0900

    [추가] #user 입력 값 변경, 페이지 대상 텍스트 가져와 단어 등장 횟수 카운트

--- week1/jimin_week1/chromeExtension_workspace/script.js
+++ week1/jimin_week1/chromeExtension_workspace/script.js
@@ -1,3 +1,19 @@
-chrome.tabs.executeScript ({
-    code: 'var bodyText = document.querySelector("body").innerText;alert(bodyText);'
+document.querySelector('#user').addEventListener('change', function () {
+    var user = document.querySelector('#user').value;
+
+    chrome.tabs.executeScript({
+        code: 'document.querySelector("body").innerText'
+    }, function (result) {
+
+        var bodyText = result[0];
+        var bodyNum = bodyText.split(' ').length;
+        var myNum = bodyText.match(new RegExp('\\b(' + user + ')\\b', 'gi')).legnth;
+
+        var per = myNum / bodyNum * 100;
+        per = per.toFixed(1);
+
+        document.querySelector('#result').innerText = myNum + '/' + bodyNum + '(' + (per) + '%)';
+
+    });
+
 });
