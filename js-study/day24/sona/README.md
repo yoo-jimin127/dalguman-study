b1e79280c45ba9687b2e7a9f2bd488323ba694e9
ballsona0925 <ballsona0925@naver.com>
2022-02-15 20:13:20 +0900

    update : Edit sona's day24 project

--- js-study/day24/sona/README.md
+++ js-study/day24/sona/README.md
@@ -1,3 +1,10 @@
 # Day24
 
 ---
+
+Top navigation 스크롤시 상단에 고정시키기.
+
+- css 오타 때문에 몇시간 때문에 애먹었다.. (오타 못찾아서 소스를 두번 엎음..)
+- navigation 고정 전에는 상단 메뉴가 4개였다가, 고정 이후에는 5개로 메뉴 개수를 바꿀 건데, 바꾸는 과정에 transition을 넣어야했다.
+  - 보통 항목을 숨길때 display:none을 사용했는데, transition을 주기 위해 max-width를 사용했다.
+  - 숨겨져있을 땐 max-width:0; overhidden:none이었다가, 보여져야할 때 max-width: 높은 수; 를 주는 방법.
--- js-study/day24/sona/script.js
+++ js-study/day24/sona/script.js
@@ -1,15 +1,14 @@
 const nav = document.querySelector('#main');
 let topOfNav = nav.offsetTop;
 
-function navHandler() {
-    if (window.scrollY >= topOfNav) {
-        document.body.style.paddingTop = nav.offsetHeight + 'px';
-        document.body.classList.add('fixed');
-    } else {
-        document.body.classList.remove('fixed');
-        document.body.style.paddingTop = 0;
-    }
-    console.log('1');
+function navHandler (){
+ if(window.scrollY >= topOfNav){
+     document.body.style.paddingTop = nav.offsetHeight + 'px';
+     document.body.classList.add('fixed-nav');
+ } else{
+     document.body.classList.remove('fixed-nav');
+     document.body.style.paddingTop = 0;
+ }
 }
 
 window.addEventListener('scroll', navHandler);
